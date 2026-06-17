9cd97b23548be1e93178c737a4cd4c346aa8ff44
ekerenkoen-prog <ekeren.koen@gmail.com>
2026-06-17 13:25:27 +1000

    Initial Finance OS setup

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Finance OS",
+    page_icon="💰",
+    layout="wide"
+)
+
+st.title("Finance OS")
+st.caption("Building Financial Freedom")
